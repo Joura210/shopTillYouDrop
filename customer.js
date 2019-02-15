3439af2f90279693754f9e31ad45bb3bc8b16b9e
joura <martinezluisjorge86@gmail.com>
2019-02-15 13:14:02 -0500

    have database linked and showing database objects.

---
+++ customer.js
@@ -0,0 +1,36 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'Localhost',
+
+    port: 3306,
+
+    user:"root",
+
+    password: "Notanother1",
+
+    database: "bamazon_db"
+});
+
+connection.connect(function (err){
+    if (err) throw err;
+    console.log("Connected as id " + connection.threadId + "\n");
+    start();
+});
+
+var start = function() {
+    connection.query('SELECT * FROM products', function(err, res) {
+        inquirer.prompt({
+            name: 'product',
+            type: 'rawlist',
+            choices: function(value) {
+                var choiceArray = [];
+                for (var i = 0; i < res.length; i++) {
+                    choiceArray.push(res[i].product);
+                }
+                return choiceArray;
+            }
+        })
+    });
+}      
